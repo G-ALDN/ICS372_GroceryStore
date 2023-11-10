e9d5e39251b2d457596421e0b1c004a623a03418
prestofresco <prestonkuhn@gmail.com>
2023-11-10 12:48:21 -0600

    member and memberlist implemented

--- src/MemberList.java
+++ src/MemberList.java
@@ -1,4 +1,34 @@
+import java.util.ArrayList;
 
 public class MemberList {
 
+	private ArrayList<Member> memberList;
+	private int totalMembers;
+	
+	public MemberList() {
+		this.memberList = new ArrayList<Member>();
+		this.totalMembers = 0;
+	}
+	
+	public void addMember(Member member) {
+		this.memberList.add(member);
+	}
+	
+	public Member get(int index) {
+		return memberList.get(index);
+	}
+	
+	// method to find and return the member with the matching memberID
+	// if the member is found in the memberList, the member object is returned.
+	// if the member is not found, null is returned. 
+	public Member getMember(int memberID) {
+		for (Member member : memberList) {
+			if (member.getMemberID() == memberID) {
+				return member;
+			}
+		}
+		// member was not found
+		return null;
+	}
 }
+
