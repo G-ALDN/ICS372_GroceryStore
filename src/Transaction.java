30b2160fce31a4dd1d4f0e9ed1a2ad778e0ee520
G-ALDN <128417694+G-ALDN@users.noreply.github.com>
2023-11-13 08:50:41 -0600

    Cart LineItem and Transaction classes

--- src/Transaction.java
+++ src/Transaction.java
@@ -1,4 +1,31 @@
+import java.util.ArrayList;
+import java.time.*;
 
 public class Transaction {
+    int memberID;
+    ArrayList<LineItem> lineItemList;
+    int totalProducts;
+    ZonedDateTime dateOfSale;
+    double total;
+
+    public Transaction(int memberID, ArrayList<LineItem> lineItemList, int totalProducts, double total) {
+        this.memberID = memberID;
+        this.lineItemList = lineItemList;
+        this.totalProducts = totalProducts;
+        this.total = total;
+        dateOfSale = java.time.ZonedDateTime.now();
+    }
+
+    public void print() {
+        System.out.println("Transaction                        MemberID: " + memberID);
+        System.out.println(".......................................................");
+        for (LineItem l : lineItemList) {
+            l.print();
+        }
+        System.out.println("\nTotal: " + total);
+        System.out.println(
+                "Date: " + dateOfSale.getYear() + "-" + dateOfSale.getMonthValue() + "-" + dateOfSale.getDayOfMonth());
+
+    }
 
 }
